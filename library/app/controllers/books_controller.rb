dfa7a1a715c70e6f65110610f45f63f4fecaf55d
sherly50825 <sherly50825@gmail.com>
2017-05-05 09:46:17 -0400

    finish add book feature

--- library/app/controllers/books_controller.rb
+++ library/app/controllers/books_controller.rb
@@ -7,5 +7,22 @@ class BooksController < ApplicationController
     @book = Book.new
   end
   
+  def create
+    @book = Book.new(book_parms)
+    
+    if @book.save
+      redirect_to books_path
+    else
+      renden 'new'
+    end
+  end
+  
+  
+  
+  private
+    def book_parms
+      params.require("book").permit(:name,:content,:author,:price)
+    end
+   
 
 end
